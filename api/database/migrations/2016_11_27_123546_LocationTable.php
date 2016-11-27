66d7ce0f65a1c02151a31e629999ea0858afa7ab
Huang Wei-Hao <demotascha@gmail.com>
2016-11-27 20:56:43 +0800

    add migration for locations

---
+++ api/database/migrations/2016_11_27_123546_LocationTable.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class LocationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('city');
+            $table->double('lat');
+            $table->double('lag');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+}
