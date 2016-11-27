7152db2784403495c7c051addbe84d735a5d3fbb
Huang Wei-Hao <demotascha@gmail.com>
2016-11-27 22:15:12 +0800

    run `php artisan app:name Watergo`

--- api/app/Http/Controllers/Auth/LoginController.php
+++ api/app/Http/Controllers/Auth/LoginController.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace Watergo\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use Watergo\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
--- api/app/Providers/AppServiceProvider.php
+++ api/app/Providers/AppServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Providers;
+namespace Watergo\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
