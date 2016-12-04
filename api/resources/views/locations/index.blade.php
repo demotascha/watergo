53a514e553a9ef4caa1f4c186bb63aa7e418c151
Huang Wei-Hao <demotascha@gmail.com>
2016-12-04 13:27:01 +0800

    add form when you rightclick

--- api/resources/views/locations/index.blade.php
+++ api/resources/views/locations/index.blade.php
@@ -45,15 +45,25 @@
         });
         // This event listener calls addMarker() when the map is clicked.
         google.maps.event.addListener(map, 'rightclick', function(event) {
+            var html = "<table>" +
+                    "<tr><td>Name:</td> <td><input type='text' id='name'/> </td> </tr>" +
+                    "<tr><td>Address:</td> <td><input type='text' id='address'/></td> </tr>" +
+                    "<tr><td>Type:</td> <td><select id='type'>" +
+                    "<option value='bar' SELECTED>bar</option>" +
+                    "<option value='restaurant'>restaurant</option>" +
+                    "</select> </td></tr>" +
+                    "<tr><td></td><td><input type='button' value='Save & Close' onclick=''/></td></tr>";
+            infowindow = new google.maps.InfoWindow({
+                content: html
+            });
             var marker = new google.maps.Marker({
                 position: event.latLng,
-                infoWindow: {
-                    content: '<p>'+ 'jskfld' +'</p>'
-                }
             });
-            //markers.push(marker);
+
             if (markerCluster) {
                 markerCluster.addMarker(marker, true);
+                markerCluster.redraw();
+                infowindow.open(map, marker);
             }
         });
         var markerCluster = new MarkerClusterer(map, markers, {imagePath: '../images/m'});
